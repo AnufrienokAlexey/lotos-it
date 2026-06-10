689620f08027ba92b22ae8bd38147f5c66d15de8
Alexey Anufrienok <alex.anufrienok@gmail.com>
2026-06-10 18:21:16 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://www.amocrm.ru/developers/content/crm_platform/api-reference';
+const buttonGetContacts = document.querySelector('.button-get-contacts');
+console.log(buttonGetContacts);
