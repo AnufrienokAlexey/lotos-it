5ac05ee279b4c70e5b627a957f29c4656b7ebdcd
Alexey Anufrienok <alex.anufrienok@gmail.com>
2026-06-19 14:40:24 +0300

    swiper

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-const API_URL = 'https://www.amocrm.ru/developers/content/crm_platform/api-reference';
-const buttonGetContacts = document.querySelector('.button-get-contacts');
-console.log(buttonGetContacts);
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
